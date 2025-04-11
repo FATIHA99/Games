f155a719b4f31dd606af323a276fe9cf4a5ccab9
FATIHA99 <fatihhaa27@gmail.com>
2025-04-11 18:39:05 +0100

    done

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -1,12 +1,14 @@
+"use client"
 import styles from './Hero.module.css';
 
+
 export default function Hero() {
+
   return (
     <div className={styles.container}>
-      <h1 className={` Kid Games ${styles.title} `}>WELCOME TO GAMES 2025</h1>
-      <p className={` game ${styles.description}`}>Explore the best games and have fun!</p>
-      <a  href="https://locked3.com/cl/i/8dkdjd" className={` ${styles.customBtn} `}>Click here </a>
-
+      <h1 className={`Kid Games ${styles.title} ${styles.animateTitle}`}>WELCOME TO <span className={` ${styles.Herospan} `}> GAMES 2025 </span></h1>
+      <p className={`game ${styles.description} ${styles.animateDescription}`}>Explore the best games and have fun!</p>
+      <a href="https://locked3.com/cl/i/8dkdjd" className={`${styles.customBtn}`}>Click here</a>
     </div>
   );
 }
